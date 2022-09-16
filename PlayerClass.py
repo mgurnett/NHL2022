7a0e3b6a8f2f6d27f3a6bc64e3d652fa0b79902a
mgurnett <mgurnett@gmail.com>
2022-09-16 16:17:00 -0600

    more working

--- PlayerClass.py
+++ PlayerClass.py
@@ -5,8 +5,9 @@ from LeagueClass import *
 
 class Player:
     ''' The Player class for a single player '''
-    def __init__ (self, id):
+    def __init__ (self, id, season):
         self.id = id
+        self.season = season
         self.first_name = ''
         self.lastName = ''
         self.primaryNumber = ''
@@ -22,13 +23,30 @@ class Player:
         self.nationality = ''
         self.rookie = ''
         self.shootsCatches = ''
+        
+        self.assists = 0
+        self.goals = 0
+        self.pim = 0
+        self.shots = 0
+        self.games = 0
+        self.powerPlayGoals = 0
+        self.powerPlayPoints = 0
+        self.penaltyMinutes = 0
+        self.shotPct = 0
+        self.gameWinningGoals = 0
+        self.overTimeGoals = 0
+        self.shortHandedGoals = 0
+        self.shortHandedPoints = 0
+        self.plusMinus = 0
+        self.points = 0
+
         self.load_player_data()
 
     def __str__ (self):
         player_info = (f'{self.first_name} {self.lastName} (#{self.primaryNumber}) {self.primaryPosition_name} \
 Active:{self.rosterStatus} & is {self.currentAge} years old')
-
-        return player_info
+        player_stats = (f'\nHis stats are: {self.points} points (G-{self.goals} & A-{self.assists}) in {self.games} games')
+        return player_info + player_stats
 
     def load_player_data (self):
         url = (f'people/{str(self.id)}')
@@ -49,17 +67,38 @@ Active:{self.rosterStatus} & is {self.currentAge} years old')
         self.nationality = data_people.get('nationality')
         self.rookie = data_people.get('rookie')
         self.shootsCatches = data_people.get('shootsCatches')
+        
+        url = (f'people/{self.id}/stats?stats=statsSingleSeason&season={self.season}')
+        data = read_API (url)
+        data_stats = data['stats'][0]['splits'][0]['stat']
+        self.assists = data_stats.get('assists')
+        self.goals = data_stats.get('goals')
+        self.pim = data_stats.get('pim')
+        self.shots = data_stats.get('shots')
+        self.games = data_stats.get('games')
+        self.powerPlayGoals = data_stats.get('powerPlayGoals')
+        self.powerPlayPoints = data_stats.get('powerPlayPoints')
+        self.penaltyMinutes = data_stats.get('penaltyMinutes')
+        self.shotPct = data_stats.get('shotPct')
+        self.gameWinningGoals = data_stats.get('gameWinningGoals')
+        self.overTimeGoals = data_stats.get('overTimeGoals')
+        self.shortHandedGoals = data_stats.get('shortHandedGoals')
+        self.shortHandedPoints = data_stats.get('shortHandedPoints')
+        self.plusMinus = data_stats.get('plusMinus')
+        self.points = data_stats.get('points')
 
 if __name__ == '__main__':
-#     NHL_season = '20212022'
-    NHL_season = '20222023'
-
+    NHL_season = '20212022'
+#     NHL_season = '20222023'
+    '''
     league = load_team_info (NHL_season) # league is a <class 'list'> of <class 'Team'>
     teams = [team.to_dict() for team in league] # build a list of dicts from your objects
     json_string = json.dumps ({'teams': teams}, indent=2)  # serialize the whole thing
 #     print (json_string)
 #     write_json (json_string, f'NHL_teams_{NHL_season}_info')
-    
+    '''
+# gets every player from every team
+    '''  
     for team in league:
         print (team)
         for player in team.roster:
@@ -68,4 +107,16 @@ if __name__ == '__main__':
             except:
                 print(f"{player} is not an active player")
             else:
-                print (current_player)
+                print (current_player)'''
+            
+            
+# gets every player from one team
+    
+    current_team = Team (22, NHL_season)
+    for player in current_team.roster:
+        try:
+            current_player = Player (player, NHL_season)
+        except:
+            print(f"{player} is not an active player")
+        else:
+            print (current_player)
--- first_flask.py
+++ first_flask.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from PlayerClass import *
-from League import *
+from LeagueClass import *
 
+NHL_season = '20212022'
 app = Flask (__name__)
 
 @app.route ('/')
@@ -10,15 +11,24 @@ def index():
 
 @app.route ('/player')
 def player():
-    url = 'teams/22?season=20212022'
-    packages_json = read_API (url)
-    current_player = Player (8480802)
-
-    return str(current_player)
+    league = load_team_info (NHL_season)
+    current_team = Team (22, NHL_season)
+    print (current_team)
+    for player in current_team.roster:
+        try:
+            current_player = Player (player, NHL_season)
+        except:
+            print(f"{player} is not an active player")
+        else:
+            return str(current_player)
 
 @app.route ('/teams')
 def teams():
-    return str(get_league ())
+    league = load_team_info (NHL_season) # league is a <class 'list'> of <class 'Team'>
+    league = update_team_stats (league, NHL_season) # league is a <class 'list'> of <class 'Team'>
+    teams = [team.to_dict() for team in league] # build a list of dicts from your objects
+    json_string = json.dumps ({'teams': teams}, indent=2)  # serialize the whole thing
+    return json_string
 
 if __name__ == '__main__':
     pass
