e26f6fffdb3b4adc0422db4b160bb228b1aad887
mgurnett <mgurnett@gmail.com>
2022-09-20 12:18:21 -0600

    Update PlayerClassSplit.py

--- PlayerClassSplit.py
+++ PlayerClassSplit.py
@@ -26,8 +26,8 @@ class Player:
     def __str__ (self):
         player_info = (f'{self.id} {self.firstName} {self.lastName} (#{self.primaryNumber}) {self.primaryPosition_name} \
 Active:{self.rosterStatus} & is {self.currentAge} years old')
-        if self.primaryPosition_name = 'Goalie':
-            player_stats = (f'\n')
+        if self.primaryPosition_name == 'Goalie':
+            player_stats = (f'\nHis stats are: {self.wins} wins in {self.games} games with a {str(round(self.evenStrengthSavePercentage))}% save percentage')
         else:
             player_stats = (f'\nHis stats are: {self.points} points (G-{self.goals} & A-{self.assists}) in {self.games} games')
         return player_info + player_stats
@@ -50,41 +50,54 @@ class Skater (Player):
         self.shortHandedPoints = 0
         self.plusMinus = 0
         self.points = 0
-        
-    def get_skater_stats():
-        url = (f'people/{self.id}/stats?stats=statsSingleSeason&season={self.season}')
-        data = read_API (url)
-        data_stats = data['stats'][0]['splits'][0]['stat']
-        self.assists = data_stats.get('assists')
-        self.goals = data_stats.get('goals')
-        self.pim = data_stats.get('pim')
-        self.shots = data_stats.get('shots')
-        self.games = data_stats.get('games')
-        self.powerPlayGoals = data_stats.get('powerPlayGoals')
-        self.powerPlayPoints = data_stats.get('powerPlayPoints')
-        self.penaltyMinutes = data_stats.get('penaltyMinutes')
-        self.shotPct = data_stats.get('shotPct')
-        self.gameWinningGoals = data_stats.get('gameWinningGoals')
-        self.overTimeGoals = data_stats.get('overTimeGoals')
-        self.shortHandedGoals = data_stats.get('shortHandedGoals')
-        self.shortHandedPoints = data_stats.get('shortHandedPoints')
-        self.plusMinus = data_stats.get('plusMinus')
-        self.points = data_stats.get('points')
+        self.timeOnIce = 0
+        self.powerPlayTimeOnIce = 0
+        self.evenTimeOnIce = 0
+        self.faceOffPct = 0
+        self.shortHandedTimeOnIce = 0
+        self.blocked = 0
+        self.plusMinus = 0
+        self.points = 0
+        self.shifts = 0
+        self.timeOnIcePerGame = 0
+        self.evenTimeOnIcePerGame = 0
+        self.shortHandedTimeOnIcePerGame = 0
+        self.powerPlayTimeOnIcePerGame = 0
 
 class Goalie (Player):
     ''' The Player class for a goalie '''
     def __init__ (self, id, season):
-        pass
+        self.timeOnIce = 0
+        self.ot = 0
+        self.shutouts = 0
+        self.ties = 0
+        self.wins = 0
+        self.losses = 0
+        self.saves = 0
+        self.powerPlaySaves = 0
+        self.shortHandedSaves = 0
+        self.evenSaves = 0
+        self.shortHandedShots = 0
+        self.evenShots = 0
+        self.powerPlayShots = 0
+        self.savePercentage = 0
+        self.goalAgainstAverage = 0
+        self.games = 0
+        self.gamesStarted = 0
+        self.shotsAgainst = 0
+        self.goalsAgainst = 0
+        self.timeOnIcePerGame = 0
+        self.powerPlaySavePercentage = 0
+        self.shortHandedSavePercentage = 0
+        self.evenStrengthSavePercentage = 0
 
 def get_a_new_player (id, season):
     url = (f'people/{str(id)}')
     data = read_API (url)
     data_people = data['people'][0]
     if data_people.get('primaryPosition').get('name') == 'Goalie':
-        print (f'goalie {id} - {season}')
         current_player = Goalie (id, season)
     else:
-        print (f'skater {id} - {season}')
         current_player = Skater (id, season)
         
     current_player.id = id
@@ -105,11 +118,38 @@ def get_a_new_player (id, season):
     current_player.shootsCatches = data_people.get('shootsCatches')
     
     if current_player.primaryPosition_name == 'Goalie':
-        print (f'goalie {id} - {season}')
+            
+        url = (f'people/{id}/stats?stats=statsSingleSeason&season={season}')
+        data = read_API (url)
+        data_stats = data['stats'][0]['splits'][0]['stat']
+        current_player.timeOnIce = data_stats.get ('timeOnIce')
+        current_player.ot = data_stats.get ('ot')
+        current_player.shutouts = data_stats.get ('shutouts')
+        current_player.ties = data_stats.get ('ties')
+        current_player.wins = data_stats.get ('wins')
+        current_player.losses = data_stats.get ('losses')
+        current_player.saves = data_stats.get ('saves')
+        current_player.powerPlaySaves = data_stats.get ('powerPlaySaves')
+        current_player.shortHandedSaves = data_stats.get ('shortHandedSaves')
+        current_player.evenSaves = data_stats.get ('evenSaves')
+        current_player.shortHandedShots = data_stats.get ('shortHandedShots')
+        current_player.evenShots = data_stats.get ('evenShots')
+        current_player.powerPlayShots = data_stats.get ('powerPlayShots')
+        current_player.savePercentage = data_stats.get ('savePercentage')
+        current_player.goalAgainstAverage = data_stats.get ('goalAgainstAverage')
+        current_player.games = data_stats.get ('games')
+        current_player.gamesStarted = data_stats.get ('gamesStarted')
+        current_player.shotsAgainst = data_stats.get ('shotsAgainst')
+        current_player.goalsAgainst = data_stats.get ('goalsAgainst')
+        current_player.timeOnIcePerGame = data_stats.get ('timeOnIcePerGame')
+        current_player.powerPlaySavePercentage = data_stats.get ('powerPlaySavePercentage')
+        current_player.shortHandedSavePercentage = data_stats.get ('shortHandedSavePercentage')
+        current_player.evenStrengthSavePercentage = data_stats.get ('evenStrengthSavePercentage')
+
     else:
-        print (f'skater {id} - {season}') 
     
         url = (f'people/{id}/stats?stats=statsSingleSeason&season={season}')
+#         https://statsapi.web.nhl.com/api/v1/people/8478402/stats?stats=statsSingleSeason&season=20222023
         data = read_API (url)
         data_stats = data['stats'][0]['splits'][0]['stat']
         current_player.assists = data_stats.get('assists')
@@ -127,6 +167,19 @@ def get_a_new_player (id, season):
         current_player.shortHandedPoints = data_stats.get('shortHandedPoints')
         current_player.plusMinus = data_stats.get('plusMinus')
         current_player.points = data_stats.get('points')
+        current_player.timeOnIce = data_stats.get('timeOnIce')
+        current_player.powerPlayTimeOnIce = data_stats.get('powerPlayTimeOnIce')
+        current_player.evenTimeOnIce = data_stats.get('evenTimeOnIce')
+        current_player.faceOffPct = data_stats.get('faceOffPct')
+        current_player.shortHandedTimeOnIce = data_stats.get('shortHandedTimeOnIce')
+        current_player.blocked = data_stats.get('blocked')
+        current_player.plusMinus = data_stats.get('plusMinus')
+        current_player.points = data_stats.get('points')
+        current_player.shifts = data_stats.get('shifts')
+        current_player.timeOnIcePerGame = data_stats.get('timeOnIcePerGame')
+        current_player.evenTimeOnIcePerGame = data_stats.get('evenTimeOnIcePerGame')
+        current_player.shortHandedTimeOnIcePerGame = data_stats.get('shortHandedTimeOnIcePerGame')
+        current_player.powerPlayTimeOnIcePerGame = data_stats.get('powerPlayTimeOnIcePerGame')
     return current_player
 
 def update_player_stats ():
@@ -136,9 +189,9 @@ def update_player_stats ():
 if __name__ == '__main__':
     NHL_season = '20212022'
 #     NHL_season = '20222023'
-    
-    player = get_a_new_player (8469608, NHL_season)
-    print (player)
+#     
+#     player = get_a_new_player (8478402, NHL_season)
+#     print (player)
     '''
     league = load_team_info (NHL_season) # league is a <class 'list'> of <class 'Team'>
     teams = [team.to_dict() for team in league] # build a list of dicts from your objects
@@ -159,13 +212,13 @@ if __name__ == '__main__':
                 print (current_player)
             
             
-# gets every player from one team
+# gets every player from one team'''
     
     current_team = Team (22, NHL_season)
     for player in current_team.roster:
         try:
-            current_player = Player (player, NHL_season)
+            current_player = get_a_new_player (player, NHL_season)
         except:
             print(f"{player} is not an active player")
         else:
-            print (current_player)'''
+            print (current_player)
